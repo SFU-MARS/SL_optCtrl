407b27d3bb98d7283d66617be4e21d6a636a5296
NoNameAA <bbmmddkk@gmail.com>
2019-07-31 16:34:10 -0700

    remove lines related sample from goal and added state generator function

--- value_iteration/value_iteration.py
+++ value_iteration/value_iteration.py
@@ -10,15 +10,22 @@ class world_env(object):
         self.x = (0, 5)
         self.y = (0, 5)
 
-        self.goal_x = (4, 5)
-        self.goal_y = (4, 5)
-
         self.gravity = 9.8
 
         self.obstacle = None
 
+        ##################### Goal Env ######################        
+        self.goal_x = (4, 5)
+        self.goal_y = (4, 5)
+
+        self.goal_vx = (-10, 10)
+        self.goal_vy = (-10, 10)
+
+        self.goal_theta = (-10, 10)
+        self.goal_omega = (-10, 10)
+
         ###################### Drone ########################
-        self.phi = (-math.pi, math.pi)
+        self.theta = (-math.pi, math.pi)
         self.omega = (-math.pi * 2, math.pi * 2)
 
         self.vx = (0, 1)
@@ -36,22 +43,18 @@ class world_env(object):
 
         ############# Discreteness Coefficient ##############
         # 6D state + 2D action
-        # (x, y, vx, vy, phi, omega, t1, t2)
-        self.step_number = np.array([10, 10, 10, 10, 10, 10, 10, 10])
-        self.ranges = np.array([self.x, self.y, self.vx, self.vy, self.phi, self.omega, self.t1, self.t2])
+        # (x, y, vx, vy, theta, omega, t1, t2)
+        # self.step_number = np.array([10, 10, 10, 10, 10, 10, 10, 10])
+        self.step_number = np.array([2, 2, 2, 2, 2, 2, 2, 2])
+        self.ranges = np.array([self.x, self.y, self.vx, self.vy, self.theta, self.omega, self.t1, self.t2])
 
         #Grid is used for storing the n-D arrays regard to the discret states after cutting
         self.grid = None
 
-
         ############# Initial Points From Goal ##############
-        self.init_point = None
+        self.state = None
 
-        # How many points will sample on each dimension.
-        # 6D state + 2D action (n_x, n_y, n_vx, n_vy, n_phi, n_omega, n_t1, n_t2)
-        self.sample_number = np.array([2, 2, 2, 2, 2, 2, 2, 2])
-        self.goal_ranges = np.array([self.goal_x, self.goal_y, self.vx, self.vy, 
-                                    self.phi, self.omega, self.t1, self.t2])
+
 
     def add_obstacle(self, x1, x2, y1, y2):
         if ((x1 > x2) or (y1 > y2)):
@@ -68,7 +71,7 @@ class world_env(object):
         print("world_y: ", self.y)
         print("goal_x: ", self.goal_x)
         print("goal_y: ", self.goal_y)
-        print("phi: ", self.phi)
+        print("theta: ", self.theta)
         print("omega: ", self.omega)
         print("step: ", self.step)
 
@@ -90,26 +93,21 @@ class world_env(object):
                 self.grid = None
                 break
 
-    def sample_from_goal(self, dim = 8):
+    def state_generator(self, dim = 8):
         dim_x = [0, 2, 4, 5]  
         dim_y = [1, 3, 4, 5]
         dim_a = [6, 7]
 
-        self.init_point = np.empty((dim, self.sample_number.max()))
-
-        for i in range(dim):
-            if (self.goal_ranges[i][0] < self.goal_ranges[i][1]):
-                self.init_point[i] = np.linspace(self.goal_ranges[i][0], 
-                                                self.goal_ranges[i][1], 
-                                                self.sample_number[i]) 
-
         # refer: https://stackoverflow.com/questions/1208118/using-numpy-to-build-an-array-of-all-combinations-of-two-arrays
-        self.init_point = np.array(np.meshgrid(self.init_point[0],
-                                                self.init_point[2],
-                                                self.init_point[4],
-                                                self.init_point[5])).T.reshape(-1, len(dim_x))
+        self.state = np.array(np.meshgrid(self.grid[0],
+                                            self.grid[2],
+                                            self.grid[4],
+                                            self.grid[5])).T.reshape(-1, len(dim_x))
 
-    def find_nearest_position(self, state, dim):
+
+
+
+    def seek_nearest_position(self, state, dim):
         # This function can find the nearest position on discrete world for one state
         # The type of state is a row of numpy array, e.g. state = np.array([2, 3, 4, 5])
         # The dim stores the corresponding dimension of state. 
@@ -119,9 +117,7 @@ class world_env(object):
         return state
 
 
-    def previous_state_generator(self):
-        # print(self.grid)
-        return
+    
 
     
 
@@ -129,10 +125,6 @@ class world_env(object):
 if __name__ == "__main__":
     env = world_env()
     env.state_cutting()
-    # env.sample_from_goal()
+    env.state_generator()
 
-    x = np.array([4.2, 4.8])
-    dim = [0, 1]
-
-    env.previous_state_generator()
-    env.find_nearest_position(x, dim)
+    # env.seek_nearest_position(x, dim)
