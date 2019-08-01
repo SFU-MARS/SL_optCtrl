62e098dafcd45c8f5c62f871576265ffbbf76fa5
NoNameAA <bbmmddkk@gmail.com>
2019-07-31 19:07:03 -0700

    add lines in state_generator(), added state_evaluation() and state_check()

--- value_iteration/value_iteration.py
+++ value_iteration/value_iteration.py
@@ -12,7 +12,7 @@ class world_env(object):
 
         self.gravity = 9.8
 
-        self.obstacle = None
+        self.obstacles = None
 
         ##################### Goal Env ######################        
         self.goal_x = (4, 5)
@@ -28,8 +28,8 @@ class world_env(object):
         self.theta = (-math.pi, math.pi)
         self.omega = (-math.pi * 2, math.pi * 2)
 
-        self.vx = (0, 1)
-        self.vy = (0, 1)
+        self.vx = (-5, 5)
+        self.vy = (-5, 5)
 
         # thrust - action range
         self.t1 = (0, 0.14)
@@ -44,15 +44,26 @@ class world_env(object):
         ############# Discreteness Coefficient ##############
         # 6D state + 2D action
         # (x, y, vx, vy, theta, omega, t1, t2)
-        # self.step_number = np.array([10, 10, 10, 10, 10, 10, 10, 10])
-        self.step_number = np.array([2, 2, 2, 2, 2, 2, 2, 2])
         self.ranges = np.array([self.x, self.y, self.vx, self.vy, self.theta, self.omega, self.t1, self.t2])
+
+        # self.step_number = np.array([10, 10, 10, 10, 10, 10, 10, 10])
+        self.step_number = np.array([2, 2, 2, 2, 2, 2, 2, 2]) # used for debug
+
+        # The list of goal dimension
+        self.goals = np.array([self.goal_x, self.goal_y, self.goal_vx, self.goal_vy, self.goal_theta, self.goal_omega])
 
         #Grid is used for storing the n-D arrays regard to the discret states after cutting
         self.grid = None
 
-        ############# Initial Points From Goal ##############
+        ############# The discrete states in the world ##############
         self.state = None
+        self.value = None
+
+        self.n_state_type = 3
+        self.state_type = None
+
+        # reward = [none, obstacle or out of range, goal]
+        self.type_reward = np.array([-1, -500, 1000])
 
 
 
@@ -61,23 +72,10 @@ class world_env(object):
             print("Add obstacle error! Check parameters")
             return
 
-        if (self.obstacle is None):
-            self.obstacle = np.array([[x1, x2, y1, y2]], dtype=float)
+        if (self.obstacles is None):
+            self.obstacles = np.array([[x1, x2, y1, y2]], dtype=float)
         else:
-            self.obstacle = np.concatenate((self.obstacle, np.array([[x1, x2, y1, y2]])), axis = 0)
-
-    def world_info(self):
-        print("world_x: ", self.x)
-        print("world_y: ", self.y)
-        print("goal_x: ", self.goal_x)
-        print("goal_y: ", self.goal_y)
-        print("theta: ", self.theta)
-        print("omega: ", self.omega)
-        print("step: ", self.step)
-
-        print("\nobstacle: (min_x, min_y, max_x, max_y)")
-        for obs in self.obstacle:
-            print(obs)
+            self.obstacles = np.concatenate((self.obstacles, np.array([[x1, x2, y1, y2]])), axis = 0)
 
     def state_cutting(self, dim = 8):
         # Initialize and declare array space
@@ -104,8 +102,62 @@ class world_env(object):
                                             self.grid[4],
                                             self.grid[5])).T.reshape(-1, len(dim_x))
 
+        self.value = np.zeros(self.state.shape[0])
+
+        self.state_type = np.zeros([self.state.shape[0], self.n_state_type])
+        self.state_type = self.state_evaluation(self.state, self.state_type, dim_x)
 
 
+    def state_evaluation(self, state, state_type, dim):
+        for i, s in enumerate(state):
+            state_type[i][self.state_check(s, dim)] = 1
+            print(s, state_type[i])
+
+        return state_type
+
+    def state_check(self, s, dim):
+        # This function is used for returning the state_type of a state
+        # Also including check whether the state is out of range
+
+        # temp = (0, 1, 2) -> (none, obstacle, goal)
+        temp = 2
+
+        # Check in goal range
+        for i, d in enumerate(dim):
+            if (s[i] < self.goals[d][0] or s[i] > self.goals[d][1]):
+                temp = 0
+                break
+
+        if (temp):
+            return temp
+
+        # Check in obstacle
+        temp_obs = None
+
+        # Get the corresponding dimension of obstacles
+        # Due to dimension decomposition
+        if (dim[0] == 0):
+            temp_obs = self.obstacles[:, :2]
+        else:
+            temp_obs = self.obstacles[:, 2:]
+
+        # Check in obstacle!
+        for obs in temp_obs:
+            if (s[0] >= obs[0]  and  s[0] <= obs[1]):
+                temp = 1
+                break
+
+        if (temp):
+            return temp
+
+        temp = 0
+        # Check out of range!
+        for i, d in enumerate(dim):
+            if (s[i] < self.ranges[d][0] or s[i] > self.ranges[d][1]):
+                temp = 1
+                break
+
+        return temp
 
     def seek_nearest_position(self, state, dim):
         # This function can find the nearest position on discrete world for one state
@@ -124,7 +176,16 @@ class world_env(object):
 
 if __name__ == "__main__":
     env = world_env()
-    env.state_cutting()
-    env.state_generator()
 
+    env.add_obstacle(1,3,2,4)
+    env.add_obstacle(3,4,3,4)
+    env.state_cutting()
+    env.state_generator()    
     # env.seek_nearest_position(x, dim)
+
+
+    # TODO List
+    # state_check() function test
+    # state transition function() implement and test
+    # value iteration function() implement and test
+
