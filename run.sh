35456c2f0c700d328ed8ec195ca90f673ef861a8
xlv <xlv@cs-mars-01.cmpt.sfu.ca>
2019-11-23 15:33:57 -0800

    test gitignore with removing tracking cache

--- run.sh
+++ run.sh
@@ -29,11 +29,11 @@ BASEDIR=$(dirname "$0")
 
 
 /local-scratch/xlv/miniconda3/envs/py35_no_specific/bin/python $BASEDIR/train_ppo.py --gym_env=DubinsCarEnv-v0 --reward_type=hand_craft --algo=ppo --set_additional_goal=None \
---vf_load=no --pol_load=no
-
-
+--vf_load=yes --pol_load=no
+#
+#
 /local-scratch/xlv/miniconda3/envs/py35_no_specific/bin/python $BASEDIR/train_ppo.py --gym_env=DubinsCarEnv-v0 --reward_type=hand_craft --algo=ppo --set_additional_goal=None \
---vf_load=no --pol_load=no
+--vf_load=yes --pol_load=no
 #
 #
 #/local-scratch/xlv/miniconda3/envs/py35_no_specific/bin/python $BASEDIR/train_ppo.py --gym_env=DubinsCarEnv-v0 --reward_type=hand_craft --algo=ppo --set_additional_goal=None \
