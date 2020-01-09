9376aee8b6f263744de0fc02306aaa0eb1dcfdf8
xubo92 <lv_xubo@163.com>
2020-01-09 15:59:12 -0800

    train value NN using mpc-derived value and test it. value NN loss is still a little bit higher

--- results_plotter.py
+++ results_plotter.py
@@ -3,12 +3,15 @@ import sys, os
 import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
-
-case_type = 'difficult_case'
-exploration_type = 'linear_decay'
+import numpy as np
+# case_type = 'difficult_case'
+# exploration_type = 'linear_decay'
 
 # case_type = 'simple_case'
 # exploration_type = 'exponential_decay'
+
+case_type = 'difficult_case'
+exploration_type = 'linear_decay_nn'
 
 basedir = os.path.join(os.environ['PROJ_HOME_3'], 'runs_log_tests',
                        case_type, exploration_type)
@@ -16,10 +19,15 @@ rewards = []
 iterations = []
 hues = []
 for subfolder in os.listdir(basedir):
+    if os.path.isfile(subfolder):
+        continue
     fullpath = os.path.join(basedir, subfolder)
     cur_hue = '_'.join(subfolder.split('_')[4:])
     print("hue:", cur_hue)
-    cur_reward = pickle.load(open(os.path.join(fullpath, 'result', 'train_reward_iter_150.pkl'), 'rb'))
+
+    max_iter_idx = np.max([int(os.path.splitext(i)[0].split('_')[-1]) for i in os.listdir(os.path.join(fullpath, 'result'))])
+
+    cur_reward = pickle.load(open(os.path.join(fullpath, 'result', 'train_reward_iter_' + str(max_iter_idx) + '.pkl'), 'rb'))
 
     rewards.extend(cur_reward)
     iterations.extend(range(len(cur_reward)))
